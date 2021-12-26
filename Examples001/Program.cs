cb1977f9494063b8dd8b108f5b1f257473540b12
SMA <sma83@mail.ru>
2021-12-26 20:51:20 +0700

    Поиск количества конечностей

--- Examples001/Program.cs
+++ Examples001/Program.cs
@@ -1,9 +1,28 @@
-﻿using System;
-class PleaseSayUra
+﻿int NumberOfLegs (string animalName)
 {
-    static void Main()
+    if (animalName == "слон")
     {
-        Console.WriteLine("Ура!");
-        Console.ReadLine();
+        return 4;
+    }
+    else if (animalName == "индейка")
+    {
+        return 2;
+    }
+    else if (animalName == "устрица")
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
+int i;
+i = NumberOfLegs("индейка");
+Console.WriteLine("У индейки конечностей - " + i);
+i = NumberOfLegs("слон");
+Console.WriteLine("У слона конечностей - " + i);
+i = NumberOfLegs("устрица");
+Console.WriteLine("У устрицы конечностей- " + i);
+i = NumberOfLegs("червяк");
+Console.WriteLine("У червяка конечностей - " + i);
